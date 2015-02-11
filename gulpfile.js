312b0d165a736ad52e4e4ec97cda9849c9530fb3
Karen Tanigawa <tanigawa.karen@gmail.com>
2015-02-10 21:01:35 -1000

    mobile

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-  return gulp.src('./sass/**/*.scss')
+  return gulp.src('./sass/*.scss')
     .pipe(sass( { errLogToConsole: true } ))
     .pipe(gulp.dest('./public/css'));
 });
